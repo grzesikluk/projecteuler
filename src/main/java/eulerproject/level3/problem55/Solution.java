a88aef861d0d19e484b6d8631fb7f8cb0d992486
jukka2003 <jukka2003@gmail.com>
2016-09-14 09:19:09 +0200

    Update Solution.java
    Code for #55. Solved

--- src/main/java/eulerproject/level3/problem55/Solution.java
+++ src/main/java/eulerproject/level3/problem55/Solution.java
@@ -5,19 +5,61 @@ package eulerproject.level3.problem55;
  */
 public class Solution {
     public static final int MAX = 10000;
+    public static final int MAX_ITER = 50;
 
     public static void main(String[] args) {
+        int k=1;
+        int counter=0;
+
+        while(k<MAX) {
+            if (isLychrelNumber(k))
+                counter++;
+            k++;
+        }
+        System.out.println(counter);
 
     }
 
 
-    public static boolean isPalindrom(String s){
-        return false;
+    public static boolean isPalindrome(String s) {
+
+        for (int i = 0; i < s.length() / 2; i++)
+            if (s.charAt(i) != s.charAt(s.length() - i - 1))
+                return false;
+
+        return true;
     }
 
-    public static int reverseInt(int k) {
+    public static BigInteger reverseInt(BigInteger k) {
+        String reversed = "";
+        String original = k.toString();
 
-        return 0;
+        for (int i = original.length() - 1; i >= 0; i--)
+            reversed += original.charAt(i);
+
+        return new BigInteger(reversed);
+    }
+
+
+    public static BigInteger addNumberToItsReverse(BigInteger k) {
+        BigInteger result = new BigInteger(k.toString());
+
+        return result.add(reverseInt(k));
+    }
+
+    public static boolean isLychrelNumber(int k) {
+        BigInteger bigK = new BigInteger(new Integer(k).toString());
+        BigInteger temp = new BigInteger(bigK.toString());
+
+        for (int iteration = 1; iteration < MAX_ITER; iteration++) {
+            temp = addNumberToItsReverse(temp);
+
+            if (isPalindrome(temp.toString()))
+                return false;
+        }
+        return true;
+
+
     }
 
 
