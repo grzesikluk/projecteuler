df5bee5db4799c1f05afde119ddb2d82ad6b7591
jukka2003 <jukka2003@gmail.com>
2016-09-14 09:19:52 +0200

    Update SolutionTest.java
    Solution for #55. Tests.

--- src/test/java/eulerproject/level3/problem55/SolutionTest.java
+++ src/test/java/eulerproject/level3/problem55/SolutionTest.java
@@ -6,9 +6,29 @@ import org.junit.Test;
  * Created by Lukasz on 2016-09-13.
  */
 public class SolutionTest {
+    @Test
+    public void testIsPalindrome() throws Exception {
+
+        Assert.assertTrue(isPalindrome("123321"));
+        Assert.assertTrue(isPalindrome("1236321"));
+        Assert.assertFalse(isPalindrome("124321"));
+        Assert.assertTrue(isPalindrome("11"));
+        Assert.assertTrue(isPalindrome("2"));
+    }
 
     @Test
-    public void testIsPalindrom() throws Exception {
+    public void testReverseInt() throws Exception {
+        Assert.assertEquals(new BigInteger("4321"),reverseInt(new BigInteger("1234")));
+        Assert.assertEquals(new BigInteger("21"),reverseInt(new BigInteger("12")));
+        Assert.assertEquals(new BigInteger("1"),reverseInt(new BigInteger("1")));
+    }
+
+    @Test
+    public void testIsLychrelNumber() throws Exception {
+        Assert.assertFalse(isLychrelNumber(47));
+        Assert.assertFalse(isLychrelNumber(349));
+        Assert.assertTrue(isLychrelNumber(196));
 
     }
+
 }
