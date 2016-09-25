79675f41ab522ff97f79ba6c3cf9819f8e9e328e
jukka2003 <jukka2003@gmail.com>
2016-09-25 21:03:29 +0200

    Prolem 60. Init.

---
+++ src/main/java/eulerproject/level3/problem60/Solution.java
@@ -0,0 +1,11 @@
+package eulerproject.level3.problem60;
+
+/**
+ * Created by Lukasz on 2016-09-25.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/eulerproject/level3/problem60/SolutionTest.java
@@ -0,0 +1,8 @@
+package eulerproject.level3.problem60;
+
+/**
+ * Created by Lukasz on 2016-09-25.
+ */
+public class SolutionTest {
+
+}
