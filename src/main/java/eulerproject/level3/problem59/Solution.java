8eb75312f1f3239e8b52deb90553d135418cc924
jukka2003 <jukka2003@gmail.com>
2016-09-25 21:01:27 +0200

    Prolem 59 solved.

--- src/main/java/eulerproject/level3/problem59/Solution.java
+++ src/main/java/eulerproject/level3/problem59/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
 
     public static void readAllFiles() throws IOException {
         encodedMessage = Files.readAllLines(Paths.get(FILENAME));
-        encodedIntMessageStream = encodedMessage.stream().map(a -> Arrays.asList(a.split(","))).flatMap(a -> a.stream()).mapToInt(a->Integer.parseInt(a));
+        encodedIntMessageStream = encodedMessage.stream().map(a -> Arrays.asList(a.split(","))).flatMap(a -> a.stream()).mapToInt(a -> Integer.parseInt(a));
         encodedIntMessage = encodedIntMessageStream.toArray();
 
         //read common words
@@ -34,33 +34,71 @@ public class Solution {
         char[] decodedMessage = new char[intArray.length];
         int keyLen = key.length;
 
-        for(int i =0;i<intArray.length; i++){
-            decodedMessage[i] = (char) ((char)intArray[i]^key[i%keyLen]);
+        for (int i = 0; i < intArray.length; i++) {
+            decodedMessage[i] = (char) ((char) intArray[i] ^ key[i % keyLen]);
         }
         return decodedMessage;
     }
 
     public static boolean checkIfDecodedProperly(char[] message) throws IOException {
-        int counter=0;
+        int counter = 0;
         String str = new String(message);
 
-        for(String s:commonWords) {
-            if (str.contains(s))
+        for (String s : commonWords) {
+            if (str.toLowerCase().contains(s.toLowerCase()))
                 counter++;
         }
 
-        return (double)counter/(double)commonWords.size()>THRESHOLD;
+        return (double) counter / (double) commonWords.size() > THRESHOLD;
+    }
+
+    public static char[] getNextKey(char[] key) {
+
+        char[] nextKey = new char[key.length];
+
+        for (int i = 0; i < nextKey.length; i++)
+            nextKey[i] = key[i];
+
+        int k = nextKey.length - 1;
+
+        while (k >= 0) {
+            if (nextKey[k] < 'z') {
+                nextKey[k] = (char) (nextKey[k] + 1);
+                return nextKey;
+            } else {
+                nextKey[k] = 'a'; //reset
+                k--;
+            }
+        }
+
+        return null;
+
+    }
+
+    public static long sumOfAllAscii (char[] message ){
+        long result = 0;
+
+        for(int i=0;i<message.length;i++)
+            result+=message[i];
+        return result;
 
     }
 
 
     public static void main(String[] args) throws IOException {
         readAllFiles();
+        char[] key = new char[]{'a', 'a', 'a'};
+        char[] decodedMessage = null;
 
-        char[] message = decodeMesage(new char[]{'a','b','c'},encodedIntMessage);
+        while ((key = getNextKey(key)) != null) {
+            decodedMessage = decodeMesage(key, encodedIntMessage);
 
-        if(checkIfDecodedProperly(message))
-            System.out.println(message);
+            if (checkIfDecodedProperly(decodedMessage)) {
+                System.out.println(decodedMessage);
+                System.out.println(sumOfAllAscii(decodedMessage) );
+            }
+
+        }
 
     }
 }
--- src/test/java/eulerproject/level3/problem59/SolutionTest.java
+++ src/test/java/eulerproject/level3/problem59/SolutionTest.java
@@ -10,6 +10,14 @@ import java.io.IOException;
  * Created by Lukasz on 2016-09-25.
  */
 public class SolutionTest {
+    @Test
+    public void testGetNextKey() throws Exception {
+        Assert.assertArrayEquals(new char[]{'a','a','b'},Solution.getNextKey(new char[]{'a','a','a'}));
+        Assert.assertArrayEquals(new char[]{'a','b','a'},Solution.getNextKey(new char[]{'a','a','z'}));
+        Assert.assertArrayEquals(new char[]{'b','a','a'},Solution.getNextKey(new char[]{'a','z','z'}));
+        Assert.assertNull(Solution.getNextKey(new char[]{'z','z','z'}));
+    }
+
     @Before
     public void init() throws IOException {
         Solution.readAllFiles();
