5ce1c90f351709634b5328d2e04218c3ce498d10
jukka2003 <jukka2003@gmail.com>
2016-11-03 09:13:42 +0100

    Update Solution.java
    Problem 72. Init.

--- src/main/java/eulerproject/level4/problem79/Solution.java
+++ src/main/java/eulerproject/level4/problem79/Solution.java
@@ -1,7 +1,35 @@
 package eulerproject.level4.problem79;
 
-/**
- * Created by Lukasz on 2016-11-02.
- */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
+    private static final String FILENAME = "C:\\Users\\grzesikl\\IdeaProjects\\ProjectEuler\\src\\main\\java\\eulerproject\\level4\\problem79\\keylog.txt";
+    private static Set<String> codesArray;
+
+
+    public static void main(String[] args) throws IOException {
+
+        getFileContent(FILENAME);
+        System.out.println(decodePassword());
+
+    }
+
+
+    private static void getFileContent(String fileName) throws IOException {
+        Stream<String> lines = Files.lines(Paths.get(fileName));
+        codesArray = lines.collect(Collectors.toSet());
+        lines.close();
+    }
+
+    public static String decodePassword() {
+
+        return "";
+    }
+
+
 }
