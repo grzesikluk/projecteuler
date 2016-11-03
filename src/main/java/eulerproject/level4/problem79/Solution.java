a1d4c4a1dddf9d9f21445b533f0b74f967dbfbfa
jukka2003 <jukka2003@gmail.com>
2016-11-03 21:28:46 +0100

    Init of problem 79

--- src/main/java/eulerproject/level4/problem79/Solution.java
+++ src/main/java/eulerproject/level4/problem79/Solution.java
@@ -8,9 +8,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Solution {
-    private static final String FILENAME = "C:\\Users\\grzesikl\\IdeaProjects\\ProjectEuler\\src\\main\\java\\eulerproject\\level4\\problem79\\keylog.txt";
+    private static final String FILENAME = "C:\\Users\\Lukasz\\IdeaProjects\\projecteuler\\src\\main\\java\\eulerproject\\level4\\problem79\\keylog.txt";
     private static Set<String> codesArray;
-
 
     public static void main(String[] args) throws IOException {
 
@@ -20,15 +19,40 @@ public class Solution {
     }
 
 
-    private static void getFileContent(String fileName) throws IOException {
+    public static void getFileContent(String fileName) throws IOException {
         Stream<String> lines = Files.lines(Paths.get(fileName));
         codesArray = lines.collect(Collectors.toSet());
         lines.close();
     }
 
     public static String decodePassword() {
+        String result;
 
-        return "";
+        //pick first elem
+        result = codesArray.stream().collect(Collectors.toList()).get(0);
+
+        char middleChar = 'x';
+        int i = 0;
+
+        while (i < result.length()-1) {
+            while (((middleChar = getMiddleChar(result.charAt(i), result.charAt(i + 1))) != ' ')) {
+                result = result.substring(0,i+1) + middleChar + result.substring(i+1,result.length());
+            }
+            i++;
+        }
+
+        return result;
+    }
+
+
+    public static char getMiddleChar(char a, char b) {
+
+        for (String s : codesArray) {
+            if (s.charAt(0) == a && s.charAt(2) == b)
+                return s.charAt(1);
+        }
+
+        return ' ';
     }
 
 
---
+++ src/test/java/eulerproject/level4/problem79/SolutionTest.java
@@ -0,0 +1,41 @@
+package eulerproject.level4.problem79;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static eulerproject.level4.problem79.Solution.decodePassword;
+import static eulerproject.level4.problem79.Solution.getFileContent;
+import static eulerproject.level4.problem79.Solution.getMiddleChar;
+
+/**
+ * Created by Lukasz on 2016-11-03.
+ */
+public class SolutionTest {
+
+    @Before
+    public void init() throws IOException {
+        getFileContent("C:\\Users\\Lukasz\\IdeaProjects\\projecteuler\\src\\test\\java\\eulerproject\\level4\\problem79\\testfile.txt");
+    }
+
+    @Test
+    public void testGetMiddleChar() throws Exception {
+
+        Assert.assertEquals('2',getMiddleChar('1','3'));
+        Assert.assertEquals('4',getMiddleChar('3','5'));
+        Assert.assertEquals('8',getMiddleChar('7','9'));
+        Assert.assertEquals(' ',getMiddleChar('1','2'));
+    }
+
+    @Test
+    public void testDecodePassword() throws Exception {
+
+        Assert.assertEquals("123456789",decodePassword());
+
+    }
+
+
+
+}
