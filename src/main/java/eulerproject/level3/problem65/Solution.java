b05903ba042e88a2b9d28865bdbe0595a3183d19
jukka2003 <jukka2003@gmail.com>
2016-11-07 21:51:48 +0100

    Problem 89 solved.

---
+++ src/main/java/eulerproject/level3/problem65/Solution.java
@@ -0,0 +1,10 @@
+package eulerproject.level3.problem65;
+
+/**
+ * Created by Lukasz on 2016-11-07.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/eulerproject/level3/problem65/SolutionTest.java
@@ -0,0 +1,14 @@
+package eulerproject.level3.problem65;
+
+import org.junit.Test;
+
+/**
+ * Created by Lukasz on 2016-11-07.
+ */
+public class SolutionTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+
+}
