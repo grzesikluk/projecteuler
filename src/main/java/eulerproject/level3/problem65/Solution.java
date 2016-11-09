4dad7bf79500b21c852795521a251315a29dbcd9
jukka2003 <jukka2003@gmail.com>
2016-11-09 21:35:09 +0100

    Problem 65 init.

--- src/main/java/eulerproject/level3/problem65/Solution.java
+++ src/main/java/eulerproject/level3/problem65/Solution.java
@@ -1,10 +1,35 @@
 package eulerproject.level3.problem65;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Lukasz on 2016-11-07.
  */
 public class Solution {
+
     public static void main(String[] args) {
 
     }
+
+
+    public static List<Integer> getConvergentsListForEulerConstant(int max) {
+        List<Integer> result = new LinkedList<>();
+
+        result.add(new Integer(2));
+
+        int i=1;
+
+        while(i<max) {
+            if ((i+1)%3 == 0)
+                result.add(new Integer(2*(i-1)));
+            else
+                result.add(new Integer(1));
+            i++;
+        }
+
+        return result;
+    }
+
+
 }
--- src/test/java/eulerproject/level3/problem65/SolutionTest.java
+++ src/test/java/eulerproject/level3/problem65/SolutionTest.java
@@ -1,14 +1,33 @@
 package eulerproject.level3.problem65;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static eulerproject.level3.problem65.Solution.getConvergentsListForEulerConstant;
 
 /**
  * Created by Lukasz on 2016-11-07.
  */
 public class SolutionTest {
     @Test
-    public void testMain() throws Exception {
+    public void testGetConvergentsListForEulerConstant() throws Exception {
+        List<Integer> list = Arrays.asList(new Integer[]{new Integer(2),new Integer(1),new Integer(2),new Integer(1)});
+
+        Assert.assertEquals(list,getConvergentsListForEulerConstant(list.size()));
 
     }
 
+    @Test
+    public void testGetConvergentsListForEulerConstant2() throws Exception {
+        List<Integer> list = Arrays.asList(new Integer[]{new Integer(2),new Integer(1),new Integer(2),new Integer(1),
+                new Integer(1),new Integer(4),new Integer(1),
+                new Integer(1),new Integer(6),new Integer(1),
+                new Integer(1),new Integer(8),new Integer(1)});
+
+        Assert.assertEquals(list,getConvergentsListForEulerConstant(list.size()));
+
+    }
 }
