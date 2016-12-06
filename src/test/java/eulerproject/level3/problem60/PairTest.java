4e74b609fae109ae0738e4874f9a3dcccd605196
jukka2003 <jukka2003@gmail.com>
2016-12-06 17:57:16 +0100

    Update PairTest.java

--- src/test/java/eulerproject/level3/problem60/PairTest.java
+++ src/test/java/eulerproject/level3/problem60/PairTest.java
@@ -8,13 +8,17 @@ import org.junit.Test;
  */
 public class PairTest {
     @Test
+    public void getConcatenatedOfTwoPairs() throws Exception {
+        Assert.assertArrayEquals(new int[]{13,31,24,42,14,32,23,41}, new Pair(1,2).getConcatenatedOfTwoPairs(new Pair(3,4)));
+    }
+
+    @Test
     public void isOneDigitSame() throws Exception {
 
         Assert.assertTrue(new Pair(234,533).isOneDigitSame(new Pair(533,21)));
         Assert.assertFalse(new Pair(214,513).isOneDigitSame(new Pair(533,21)));
         Assert.assertFalse(new Pair(234,533).isOneDigitSame(new Pair(53,1)));
         Assert.assertTrue(new Pair(234,234).isOneDigitSame(new Pair(234,234)));
-        Assert.assertTrue(new Pair(7,19).isOneDigitSame(new Pair(97,213)));
 
     }
 
