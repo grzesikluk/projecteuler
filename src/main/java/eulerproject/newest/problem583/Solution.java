5010fb66a96c40508966c12f725ad5fe49e34bf9
jukka2003 <jukka2003@gmail.com>
2016-12-28 14:16:02 +0100

    Update Solution.java

--- src/main/java/eulerproject/newest/problem583/Solution.java
+++ src/main/java/eulerproject/newest/problem583/Solution.java
@@ -5,30 +5,40 @@ package eulerproject.newest.problem583;
  */
 public class Solution {
     private static final int MAX_PERIMETER = 10000;
-    private static long result = 0;
 
 
     public static void main(String[] args) {
+        long resultOld1=0, resultOld2=0;
 
-        for (int x = 1; x < MAX_PERIMETER/2; x++)
-            for (int y = 1; y < (MAX_PERIMETER-2*x)/2; y++) {
+        for (int i = 2000; i < 3000; i++) {
 
-                for (int flap = x / 2; flap < y*2; flap++) {
-                    HeronEnvelope heronEnvelope = new HeronEnvelope(x, y, flap);
+            resultOld1 = getSolution(i);
+            if(resultOld1 != resultOld2) {
+                System.out.println(i + " " + (resultOld1));
+                resultOld2 = resultOld1;
+            }
 
-                    if (heronEnvelope.checkIfProperEnv()) {
-                        if (heronEnvelope.getPerimeter() <= MAX_PERIMETER && heronEnvelope.checkIfAllDiagonalsIntegral() ) {
-                            System.out.println(heronEnvelope);
-                            result+=heronEnvelope.getPerimeter();
-                        }
-                    } else {
-                        break;
-                    }
+        }
+
+    }
+
+    public static int getSolution(int max) {
+        int result = 0;
+
+        for (int x = 1; x < max / 2; x++)
+            for (int y = 1; y < (max - 2 * x) / 2; y++) {
+                int maxZ = (int) Math.sqrt(x * x / 4 + y * y);
+
+                for (int z = x / 2; z <= maxZ && 2*z+2*y+x <= max; z++) {
+                    HeronEnvelope heronEnvelope = new HeronEnvelope(x, y, z);
+
+                    if ( heronEnvelope.checkIfAllDiagonalsIntegral())
+                        result += heronEnvelope.getPerimeter();
                 }
 
 
             }
-        System.out.println(result);
-
+        return result;
     }
+
 }
