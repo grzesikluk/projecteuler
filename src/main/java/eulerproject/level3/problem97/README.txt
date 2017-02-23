17fdfd3a102fb97ecc31564b93b37c6d87474f12
jukka2003 <jukka2003@gmail.com>
2017-02-23 22:08:53 +0100

    Problem 97 solved.

--- src/main/java/eulerproject/level3/problem97/README.txt
+++ src/main/java/eulerproject/level3/problem97/README.txt
@@ -4,3 +4,5 @@ it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the
 However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×2^7830457+1.
 
 Find the last ten digits of this prime number.
+----
+https://pl.wikipedia.org/wiki/Arytmetyka_modularna
--- src/main/java/eulerproject/level3/problem97/Solution.java
+++ src/main/java/eulerproject/level3/problem97/Solution.java
@@ -1,7 +1,36 @@
 package eulerproject.level3.problem97;
 
-/**
- * Created by grzesikl on 22/02/2017.
- */
 public class Solution {
+    public static final long MAX = 10000000000L;
+
+    public static void main(String[] args) {
+        //28433×2^7830457+1.
+        long p = powerModular(MAX, 2, 7830457);
+        long r = multiplyModular(MAX, 28433, p);
+
+
+        System.out.println(addModular(MAX,r, 1));
+    }
+
+    public static long addModular(long modulo, long a, long b) {
+        return (a + b) % modulo;
+    }
+
+    public static long multiplyModular(long modulo, long a, long b) {
+        return (a * b) % modulo;
+    }
+
+    public static boolean equalsModular(long modulo, long a, long b) {
+        return a % modulo == b % modulo;
+    }
+
+    public static long powerModular(long modulo, long base, long power) {
+        long result = base;
+
+        for (int i = 1; i < power; i++)
+            result = multiplyModular(modulo, result, base);
+
+        return result;
+
+    }
 }
