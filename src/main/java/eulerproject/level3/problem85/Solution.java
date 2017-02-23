1093574d4053e804d44223f6778c6ad041005c0d
jukka2003 <jukka2003@gmail.com>
2017-02-23 21:48:50 +0100

    Problem 85 solved.

--- src/main/java/eulerproject/level3/problem85/Solution.java
+++ src/main/java/eulerproject/level3/problem85/Solution.java
@@ -4,36 +4,29 @@ package eulerproject.level3.problem85;
  * Created by grzesikl on 23/02/2017.
  */
 public class Solution {
-    public final static int MAX = 100000;
+    public final static int MAX = 1000;
     public final static int SEARCHED = 2000_000;
 
     public static void main(String[] args) {
-        int m_found, n_found;
-        long distance;
-
-        m_found = 1;
-        n_found = 1;
-        distance = SEARCHED;
+        long mFound = 1;
+        long nFound = 1;
+        long distance = SEARCHED;
 
         for (int i = 0; i < MAX; i++) {
             for (int j = 0; j < MAX; j++) {
                 long noRect = getNoOfRectangles(i, j);
 
                 if (distance > calcDistance(noRect)) {
-                    m_found = i;
-                    n_found = j;
+                    mFound = i; nFound = j; //store both
                     distance = calcDistance(noRect);
-                    System.out.println(m_found + " " + n_found + " " + noRect);
                 }
             }
         }
 
-        System.out.println("Result: " + m_found + " " + n_found + " " +m_found * n_found);
-
-
+        System.out.println("Result: " + mFound + " " + nFound + " " +mFound * nFound);
     }
 
-    public static long getNoOfRectangles(int m, int n) {
+    public static long getNoOfRectangles(long m, long n) {
         return m * (m + 1) * n * (n + 1) / 4;
     }
 
