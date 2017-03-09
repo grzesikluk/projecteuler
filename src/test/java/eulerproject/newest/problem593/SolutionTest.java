ea6fea41cb5e00b2a5d2fc890948fdc86daee568
Lukas Grzesik <jukka2003@gmail.com>
2017-03-09 13:11:16 +0100

    Update SolutionTest.java

--- src/test/java/eulerproject/newest/problem593/SolutionTest.java
+++ src/test/java/eulerproject/newest/problem593/SolutionTest.java
@@ -1,10 +1,7 @@
 package eulerproject.newest.problem593;
 
 import eulerproject.tools.primes.Primes;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 
 /**
  * Created by Lukasz on 2017-03-07.
@@ -12,39 +9,27 @@ import org.junit.Test;
 public class SolutionTest {
 
 
-    private int[] primes;
+    private static int[] primes;
 
-    @Before
-    public void init() {
-        primes = new Primes(100_000_000).asList();
+    @BeforeClass
+    public static void init() {
+        primes = new Primes(1000_000).asList();
     }
 
     @Test
-    @Ignore
-    public void testGetFValue_100() throws Exception {
+    public void getFValue_100() throws Exception {
         Assert.assertEquals(463628.5, Solution.getFValue(100, 10, primes), 0.01);
     }
 
-    @Test(timeout = 2*60*1000+30*1000)
-    @Ignore
-    public void testGetFValue_10000() throws Exception {
+    @Test(timeout = 60 * 1000)
+    public void testGetFValue_100000() throws Exception {
         Assert.assertEquals(675348207.5, Solution.getFValue(100000, 10000, primes), 0.01);
     }
 
-
     @Test
-    public void getFValueNew_100() throws Exception {
-        Assert.assertEquals(463628.5, Solution.getFValueNew(100, 10, primes), 0.01);
-    }
-
-    @Test(timeout = 45*1000)
-    public void testGetFValueNew_100000() throws Exception {
-        Assert.assertEquals(675348207.5, Solution.getFValueNew(100000, 10000, primes), 0.01);
-    }
-
-    @Test
-    public void testGetFValueNew_1000000() throws Exception {
-        Assert.assertEquals(0, Solution.getFValueNew(1000000, 100000, primes), 0.01);
+    @Ignore
+    public void testGetFValue_1000000() throws Exception {
+        Assert.assertEquals(0, Solution.getFValue(1000000, 100000, primes), 0.01);
     }
 
     @Test
@@ -74,5 +59,15 @@ public class SolutionTest {
         Assert.assertEquals(941, Solution.getS2Value(5, primes));
         Assert.assertEquals(3437, Solution.getS2Value(6, primes));
     }
+
+    @Test
+    public void testGetSValueCached() throws Exception {
+
+        for (int i = 0; i < 100; i++) {
+            double k = Solution.getFValue(100, 100 - i, primes);
+            System.out.println(k);
+        }
+    }
+
 
 }
