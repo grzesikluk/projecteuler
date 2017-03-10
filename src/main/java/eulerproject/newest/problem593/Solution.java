9adb0f6748dc4bb9ee0fd22ced1fe27cf795cec1
Lukas Grzesik <jukka2003@gmail.com>
2017-03-10 13:30:12 +0100

    Update Solution.java

--- src/main/java/eulerproject/newest/problem593/Solution.java
+++ src/main/java/eulerproject/newest/problem593/Solution.java
@@ -3,17 +3,15 @@ package eulerproject.newest.problem593;
 import eulerproject.tools.functions.Statistics;
 import eulerproject.tools.primes.Primes;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Lukasz on 2017-03-07.
  */
 public class Solution {
     private static int MAX_PRIME = 200_000_000; //well tuned
-    private static int MAX  = 10_000_000;
-    private static int MIN  = 100_000;
+    private static int MAX = 10_000_000;
+    private static int MIN = 100_000;
     private static int MODULO = 10007;
     private static Map<Integer, Integer> cachedSValues;
     private static Map<Integer, Integer> cachedS2Values;
@@ -26,11 +24,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] primes = new Primes(MAX_PRIME).asList();
-        if (!(primes.length > MAX))
-            throw new AssertionError("Primes too small"); //make sure it's big enough
-
-        System.out.println(primes.length + " " + primes[primes.length-1]);
-//        System.out.println(getFValue(MAX, MIN, primes));
+        System.out.println(getFValue(MAX, MIN, primes));
 
     }
 
@@ -39,20 +33,51 @@ public class Solution {
         if (k > primesArray.length || k < 1)
             throw new IllegalArgumentException("Wrong input value " + k);
 
-        int value = primesArray[k-1] % MODULO;
+        int value = primesArray[k - 1] % MODULO;
         int resultVal = value;
 
-        for(int i=1;i<k;i++)
+        for (int i = 1; i < k; i++)
             resultVal = (resultVal * value) % MODULO;
 
 
         return resultVal;
     }
 
+    public static int getSValueNew(int k, int[] primesArray, int modulo) {
+
+        if (k > primesArray.length || k < 1)
+            throw new IllegalArgumentException("Wrong input value " + k);
+
+        int value = primesArray[k - 1] % modulo;
+        int resultVal = 1;
+
+
+        Set<Integer> list = new LinkedHashSet<>();
+
+        int p = -1;
+
+        for (int i = 1; i <= k; i++) {
+            resultVal = (resultVal * value) % modulo;
+
+            if (list.contains(resultVal)) {
+                p = (k - i) % list.size();
+                break;
+            } else
+                list.add(resultVal);
+
+        }
+
+        if (p < 0)
+            return resultVal;
+        else
+            return (int) list.toArray()[p];
+
+    }
+
     public static int getSValueCached(int k, int[] primesArray) {
 
         if (!cachedSValues.containsKey(k)) {
-            cachedSValues.put(k, getSValue(k, primesArray));
+            cachedSValues.put(k, getSValueNew(k, primesArray, MODULO));
         }
 
         return cachedSValues.get(k);
@@ -63,19 +88,11 @@ public class Solution {
         return getSValueCached(k, primes) + getSValueCached((int) Math.floor(k / 10000) + 1, primes);
     }
 
-    public static int getS2ValueCached(int k, int[] primes) {
-        if (!cachedS2Values.containsKey(k)) {
-            cachedS2Values.put(k, getS2Value(k, primes));
-        }
-
-        return cachedS2Values.get(k);
-    }
-
     public static double medianOfS2Value(int i, int j, int[] primes) {
         int[] s2Values = new int[j - i + 1];
 
         for (int k = i; k <= j; k++)
-            s2Values[k - i] = getS2ValueCached(k, primes);
+            s2Values[k - i] = getS2Value(k, primes);
 
         return Statistics.median(s2Values);
     }
@@ -84,7 +101,7 @@ public class Solution {
         int[] s2Values = new int[j - i + 1];
 
         for (int k = i; k <= j; k++)
-            s2Values[k - i] = getS2ValueCached(k, primes);
+            s2Values[k - i] = getS2Value(k, primes);
 
         Arrays.sort(s2Values);
 
@@ -93,16 +110,16 @@ public class Solution {
 
     public static double getFValue(int n, int k, int[] primes) {
 
-        assert(primes.length >= n);
+        assert (primes.length >= n);
 
         MedianArray medianArray = new MedianArray(getMedianOfS2ValueInitArray(1, k, primes));
         double result = medianArray.median();
 
         int i = 1;
-        while (i <= n - k ) {
+        while (i <= n - k) {
             //move array, remove first, add next
-            int first = getS2ValueCached(i, primes);
-            int last = getS2ValueCached(i + k, primes);
+            int first = getS2Value(i, primes);
+            int last = getS2Value(i + k, primes);
 
             medianArray.remove(first);
             medianArray.add(last);
