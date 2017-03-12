9eef5dd63d82bcddd72bcb5a169b5569c92f050c
jukka2003 <jukka2003@gmail.com>
2017-03-12 17:41:56 +0100

    Problem 592. Optimisatoin

---
+++ src/main/java/eulerproject/newest/problem594/README.txt
@@ -0,0 +1,4 @@
+
+https://projecteuler.net/problem=594
+
+https://lipn.univ-paris13.fr/~fernique/isfahan/lectures_notes.pdf
---
+++ src/main/java/eulerproject/newest/problem594/Solution.java
@@ -0,0 +1,7 @@
+package eulerproject.newest.problem594;
+
+/**
+ * Created by Lukasz on 2017-03-12.
+ */
+public class Solution {
+}
