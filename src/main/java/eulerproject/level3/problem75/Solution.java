229d7cc9dd2fa9ff2d860ae77d6dced561e1cbb8
Lukas Grzesik <jukka2003@gmail.com>
2017-03-31 13:42:10 +0200

    Update Solution.java
    Problem 75 solved.

--- src/main/java/eulerproject/level3/problem75/Solution.java
+++ src/main/java/eulerproject/level3/problem75/Solution.java
@@ -14,66 +14,36 @@ public class Solution {
 
     }
 
+
     public static boolean checkIfIsPythLengh(long length) {
         // l = 2*k*m*(m+n)
         Set<PythagoreanTriple> tripleList = new HashSet<>();
 
         long k = 1;
 
-        while ((length % (2 * k)) == 0) {
+        while (k < length / 4) {
 
-            for (long n = 1; n <= (length / (2 * k)); n++)
+            if ((length % (2 * k)) == 0) {
 
-                for (long m = n + 1; length >= (2 * k * m * (m + n)); m++) {
-                    PythagoreanTriple triple = new PythagoreanTriple(m, n).getNext(k);
+                for (long n = 1; n <= (length / (2 * k)); n++) {
+                    long m = 0;
+                    double m1 = ((double) -n + Math.sqrt(n * n + 2 * length / k)) / 2;
 
-                    if (length == triple.length()) {
-                        tripleList.add(triple);
+                    if ((Math.floor(m1) == m1) && !Double.isInfinite(m1))
+                        m = (long) m1;
 
-                        if (tripleList.size() > 1)
-                            return false;
+                    if (m > n) {
+                        PythagoreanTriple triple = new PythagoreanTriple((long) m, n).getNext(k);
+
+                        if (length == triple.length()) {
+                            tripleList.add(triple);
+
+                            if (tripleList.size() > 1)
+                                return false;
+                        }
                     }
+
                 }
-            k++;
-
-        }
-
-        if (tripleList.size() == 1)
-            return true;
-        else
-            return false;
-
-    }
-
-    public static boolean checkIfIsPythLengh_new(long length) {
-        // l = 2*k*m*(m+n)
-        Set<PythagoreanTriple> tripleList = new HashSet<>();
-
-        long k = 1;
-
-        while ((length % (2 * k)) == 0) {
-
-            for (long n = 1; n <= (length / (2 * k)); n++) {
-                long m = 0;
-                double m1 = ((double) -n + Math.sqrt(n * n + 2 * length / k)) / 2;
-                double m2 = ((double) -n - Math.sqrt(n * n + 2 * length / k)) / 2;
-
-                if ((Math.floor(m1) == m1) && !Double.isInfinite(m1))
-                    m = (long) m1;
-                else if ((Math.floor(m2) == m1) && !Double.isInfinite(m2))
-                    m = (long) m2;
-
-                if (m > n) {
-                    PythagoreanTriple triple = new PythagoreanTriple((long) m, n).getNext(k);
-
-                    if (length == triple.length()) {
-                        tripleList.add(triple);
-
-                        if (tripleList.size() > 1)
-                            return false;
-                    }
-                }
-
             }
             k++;
 
@@ -89,8 +59,8 @@ public class Solution {
     public static long countNumberOfUniqPythLength(long maxLength) {
         long count = 0;
         for (long i = 3; i <= maxLength; i++) {
-            if (checkIfIsPythLengh_new(i)) {
-                System.out.println(i);
+            if (checkIfIsPythLengh(i)) {
+//                System.out.println(i);
                 count++;
             }
         }
