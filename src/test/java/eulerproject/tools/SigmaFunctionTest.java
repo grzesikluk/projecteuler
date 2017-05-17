4c6f7978cafe4b715c2779c07406d57a444c40c6
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-17 17:15:05 +0200

    Update SigmaFunctionTest.java

--- src/test/java/eulerproject/tools/SigmaFunctionTest.java
+++ src/test/java/eulerproject/tools/SigmaFunctionTest.java
@@ -17,6 +17,13 @@ public class SigmaFunctionTest {
 
     @Test
     public void getValue() throws Exception {
+        Assert.assertEquals(1, sigmaFunction.getValue(1, 1));
+
+        Assert.assertEquals(2, sigmaFunction.getValue(2, 0));
+        Assert.assertEquals(3, sigmaFunction.getValue(2, 1));
+
+        Assert.assertEquals(2, sigmaFunction.getValue(3, 0));
+        Assert.assertEquals(4, sigmaFunction.getValue(3, 1));
 
         Assert.assertEquals(12, sigmaFunction.getValue(140, 0));
         Assert.assertEquals(336, sigmaFunction.getValue(140, 1));
@@ -27,6 +34,14 @@ public class SigmaFunctionTest {
 
     @Test
     public void getValueModular() throws Exception {
+        Assert.assertEquals(1, sigmaFunction.getValueModular(1, 1, 1000));
+
+        Assert.assertEquals(2, sigmaFunction.getValueModular(2, 0, 1000));
+        Assert.assertEquals(3, sigmaFunction.getValueModular(2, 1, 1000));
+
+        Assert.assertEquals(2, sigmaFunction.getValueModular(3, 0, 1000));
+        Assert.assertEquals(4, sigmaFunction.getValueModular(3, 1, 1000));
+
         Assert.assertEquals(12, sigmaFunction.getValueModular(140, 0, 1000));
         Assert.assertEquals(36, sigmaFunction.getValueModular(140, 1, 300));
         Assert.assertEquals(0, sigmaFunction.getValueModular(140, 2, 100));
