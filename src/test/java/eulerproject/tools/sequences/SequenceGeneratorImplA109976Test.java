7322d76aeb2c10c90bf7a2d3a88286c69b6c8ec2
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-17 16:55:35 +0200

    Update and rename SequenceGeneratorImplA109976.java to SequenceGeneratorImplA109976Test.java

---
+++ src/test/java/eulerproject/tools/sequences/SequenceGeneratorImplA109976Test.java
@@ -0,0 +1,35 @@
+package eulerproject.tools.sequences;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by grzesikl on 17/05/2017.
+ */
+public class SequenceGeneratorImplA109976Test {
+    SequenceGenerator sequenceGenerator;
+
+    @Before
+    public void init() {
+        sequenceGenerator = new SequenceGeneratorImplA109976();
+        sequenceGenerator.init(10000L);
+
+    }
+
+    @Test
+    public void getNumber() throws Exception {
+
+        List<Long> checkValues = Arrays.asList(1L, 2L, 3L, 6L, 7L, 16L, 19L, 46L, 66L, 159L, 274L, 667L);
+
+        for (int i = 0; i < checkValues.size(); i++) {
+//            Assert.assertEquals(checkValues.get(i), new Long(sequenceGenerator.getNumber(i)));
+            System.out.println(sequenceGenerator.getNumber(i));
+        }
+
+
+    }
+
+}
