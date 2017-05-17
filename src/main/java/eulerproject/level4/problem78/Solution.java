dbb4657be5f168679f0cac1f5b057ffe74eb88cc
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-17 17:40:16 +0200

    Update Solution.java

--- src/main/java/eulerproject/level4/problem78/Solution.java
+++ src/main/java/eulerproject/level4/problem78/Solution.java
@@ -1,7 +1,29 @@
 package eulerproject.level4.problem78;
 
-/**
- * Created by Lukasz on 2017-05-14.
- */
+import eulerproject.tools.sequences.SequenceGenerator;
+import eulerproject.tools.sequences.SequenceGeneratorImplA109976;
+
 public class Solution {
+    private final static long MODULO = 1000000L;
+    private final static long MAX = 1000000L;
+
+    public static void main(String[] args) {
+        SequenceGenerator sequenceGenerator = new SequenceGeneratorImplA109976();
+        sequenceGenerator.init(MODULO);
+
+
+        long i = 1;
+
+        while (i < MAX) {
+            if (sequenceGenerator.getNumber(i) == 0) {
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+
+
+    }
+
+
 }
