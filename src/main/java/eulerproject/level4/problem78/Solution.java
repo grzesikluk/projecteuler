22bdb2340a49ea6df1bd1f4c2169b2a02d10d2b4
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-25 16:19:37 +0200

    Update Solution.java

--- src/main/java/eulerproject/level4/problem78/Solution.java
+++ src/main/java/eulerproject/level4/problem78/Solution.java
@@ -1,67 +1,20 @@
-package eulerproject.level4.problem78;
-
-import eulerproject.tools.functions.SigmaFunctionBerndtImpl;
-import eulerproject.tools.primes.Primes;
-import eulerproject.tools.sequences.SequenceGenerator;
-import eulerproject.tools.sequences.SequenceGeneratorImplA109976;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Solution {
 
-    private final static long MAX = 10_000_000L;
-//    private final static long[] MAX_ARRAY = {10L, 100L, 1_000L, 10_000L, 100_000L, 1000_000L};
-    private final static long[] MAX_ARRAY = {1000_000L};
+    public static final int MAX = 10000000;
+    public static final int MODULO = 1000000;
+
 
     public static void main(String[] args) {
 
-        Primes primes = new Primes((int)MAX);
-        primes.init();
+        PartitionsNumbers partitionsNumbers = new PartitionsNumbersModuloIntImpl(MODULO);
 
-        for (long modulo : MAX_ARRAY) {
-            SequenceGenerator sequenceGenerator = new SequenceGeneratorImplA109976(new SigmaFunctionBerndtImpl(primes));
-
-            System.out.println("Doing modulo = " + modulo);
-            sequenceGenerator.init(modulo);
-
-            List<Long> cachedValues = new ArrayList<>();
-
-
-            if (cachedValues.isEmpty()) {
-                //first iteration
-                for (long i = 1; i < MAX; i++) {
-
-                    if((i % 1000) == 0)
-                        System.out.println((double)i/MAX);
-
-                    if (sequenceGenerator.getNumber(i) == 0L) {
-                        System.out.println("Found -> " + i);
-                        cachedValues.add(i);
-                    }
-
-                }
-
-            } else {
-                List<Long> newCachedValues = new ArrayList<>();
-                newCachedValues.clear();
-
-                for(long i:cachedValues)
-                    if (sequenceGenerator.getNumber(i) == 0L)
-                        newCachedValues.add(i);
-
-                cachedValues.clear();
-                cachedValues.addAll(newCachedValues);
-
+        for(int i = 2; i< MAX; i++) {
+            if(partitionsNumbers.partitionFunction(i) == 0) {
+                System.out.println(i);
+                break;
             }
-
-            System.out.println(cachedValues);
 
         }
 
-
     }
-
-
 }
