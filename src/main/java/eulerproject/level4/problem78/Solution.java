9bf52a63114dbcdd51fd22a926e9c40c1a32ba36
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-26 11:16:53 +0200

    Update Solution.java
    Solution 78.

--- src/main/java/eulerproject/level4/problem78/Solution.java
+++ src/main/java/eulerproject/level4/problem78/Solution.java
@@ -4,17 +4,39 @@ public class Solution {
     public static final int MODULO = 1000000;
 
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+        int[] results = new int[MAX];
+        results[0] = 1;
+        int n = 1;
 
-        PartitionsNumbers partitionsNumbers = new PartitionsNumbersModuloIntImpl(MODULO);
+        while (true) {
 
-        for(int i = 2; i< MAX; i++) {
-            if(partitionsNumbers.partitionFunction(i) == 0) {
-                System.out.println(i);
+            int i = 0;
+            int pentagonalNumber = 1;
+
+            while (pentagonalNumber <= n) {
+                results[n] += getSign(i) * results[n - pentagonalNumber];
+                results[n] %= MODULO;
+
+                i++;
+                pentagonalNumber = PolygonalNumbers.getPentagonalNumber(getI(i));
+
+            }
+            if (results[n] == 0) {
+                System.out.println(n);
                 break;
             }
-
+            n++;
         }
 
+    }
+
+
+    private static int getSign(int i) {
+        return (i % 4 > 1) ? -1 : 1;
+    }
+
+    private static int getI(int i) {
+        return (i % 2 == 0) ? i / 2 + 1 : -(i / 2 + 1);
     }
 }
