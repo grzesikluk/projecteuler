375086481a019e742ea158b815ae74c199e2a6bf
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-26 16:51:19 +0200

    Create ComunityChestCards.java

---
+++ src/main/java/eulerproject/level4/problem84/ComunityChestCards.java
@@ -0,0 +1,36 @@
+package eulerproject.level4.problem84;
+
+/**
+ * Created by grzesikl on 26/05/2017.
+ */
+public class ComunityChestCards {
+    Cards cards;
+
+    public ComunityChestCards() {
+        cards = new Cards(16);
+
+        cards.addCard("GO");
+        cards.addCard("JAIL");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+
+        cards.shuffleCards();
+    }
+
+    public String getNext() {
+        return cards.getNextCard();
+    }
+
+
+
+}
