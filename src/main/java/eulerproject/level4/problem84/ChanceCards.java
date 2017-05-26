9fe4c5ef567eb9a9f754b0b09436469d955c5f69
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-26 16:53:34 +0200

    Create ChanceCards.java

---
+++ src/main/java/eulerproject/level4/problem84/ChanceCards.java
@@ -0,0 +1,32 @@
+package eulerproject.level4.problem84;
+
+public class ChanceCards {
+
+    Cards cards;
+
+    public ChanceCards() {
+        cards = new Cards(16);
+
+        cards.addCard("GO");
+        cards.addCard("JAIL");
+        cards.addCard("C1");
+        cards.addCard("E3");
+        cards.addCard("H3");
+        cards.addCard("R1");
+        cards.addCard("R");
+        cards.addCard("R");
+        cards.addCard("U");
+        cards.addCard("-3");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+        cards.addCard("");
+
+        cards.shuffleCards();
+    }
+
+    public String getNext() {
+        return cards.getNextCard();
+    }
+
+}
