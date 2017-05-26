592d7a657252869c9284eed719fbf376eb9af424
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-26 16:38:37 +0200

    Create DicePair.java

---
+++ src/main/java/eulerproject/level4/problem84/DicePair.java
@@ -0,0 +1,16 @@
+package eulerproject.level4.problem84;
+
+/**
+ * This interface should be how the state full dice will behave.
+ */
+public interface DicePair {
+
+    public int throwIt();
+
+    public boolean isThrown();
+
+    public boolean isDoubleDiceThreeTimes();
+
+    public int howManyDoubleDiceInRow();
+
+}
