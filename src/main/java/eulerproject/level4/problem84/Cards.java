2eae6132ae7399cd48cb81f3b4ba5a3b9a4b1d67
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-26 16:47:16 +0200

    Create Cards.java

---
+++ src/main/java/eulerproject/level4/problem84/Cards.java
@@ -0,0 +1,30 @@
+package eulerproject.level4.problem84;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by grzesikl on 26/05/2017.
+ */
+public class Cards {
+    private int max;
+    List<String> cardList;
+
+    public Cards(int amount) {
+        max = amount;
+        cardList = new LinkedList<>();
+    }
+
+    public void addCard(String name) {
+        if (max > cardList.size())
+            cardList.add(name);
+    }
+
+    public void shuffleCards() {
+        if (cardList.size() == max)
+            Collections.shuffle(cardList);
+    }
+
+
+}
