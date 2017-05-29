ac8a6d750218df15ab963f40ec41b477598cddd8
Lukasz Grzesik <grzesik.luk@gmail.com>
2017-05-29 16:20:22 +0200

    Update SolutionTest.java

--- src/test/java/eulerproject/level3/problem60/SolutionTest.java
+++ src/test/java/eulerproject/level3/problem60/SolutionTest.java
@@ -5,9 +5,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by Lukasz on 2016-09-25.
  */
@@ -27,48 +24,24 @@ public class SolutionTest {
 
     @Test
     public void isPrimePair_one() throws Exception {
-        Assert.assertTrue(Solution.isPrimePair(new Pair(3, 7)));
-        Assert.assertTrue(Solution.isPrimePair(new Pair(109, 673)));
-        Assert.assertTrue(Solution.isPrimePair(new Pair(3, 109)));
-        Assert.assertTrue(Solution.isPrimePair(new Pair(673, 7)));
+        Solution solution = new Solution();
+
+        Assert.assertTrue(solution.isPrimePair(new Pair(3, 7)));
+        Assert.assertTrue(solution.isPrimePair(new Pair(109, 673)));
+        Assert.assertTrue(solution.isPrimePair(new Pair(3, 109)));
+        Assert.assertTrue(solution.isPrimePair(new Pair(673, 7)));
 
     }
 
 
     @Test
-    public void isPrimePairList_true() throws Exception {
-        List<Integer> list = Arrays.asList(3, 7, 109, 673);
-        Assert.assertTrue(Solution.isPrimeList(list));
-    }
+    public void testSearchForChain() {
+        Solution solution = new Solution();
 
-    @Test
-    public void isPrimePairList_false() throws Exception {
-        List<Integer> list = Arrays.asList(3, 8, 109, 673);
-        Assert.assertFalse(Solution.isPrimeList(list));
-    }
-
-
-    @Test
-    public void convertToList() throws Exception {
-        List<Pair> list = Arrays.asList(new Pair(3, 5), new Pair(4, 5), new Pair(10, 3));
-        Assert.assertEquals(Arrays.asList(3, 4, 5, 10), Solution.convertToIntegerList(list));
+        Assert.assertEquals(10, solution.searchForChain(primes, 2, 2).stream().mapToInt( s->s).sum());
+        Assert.assertEquals(119, solution.searchForChain(primes, 3, 2).stream().mapToInt( s->s).sum());
+        Assert.assertEquals(792, solution.searchForChain(primes, 4, 2 ).stream().mapToInt( s->s).sum());
 
     }
-
-    @Test
-    public void countPrimeHits() throws Exception {
-        List<Integer> list = Arrays.asList(3, 7, 109, 673);
-        Assert.assertEquals(6, Solution.countPrimeHits(list));
-    }
-
-
-    @Test(timeout = 100000)
-    public void testCreateInitialPrimePairSet1() throws Exception {
-        Primes primes = new Primes(100);
-        primes.init();
-
-        Solution.createInitialPrimePairSet(primes.asList());
-    }
-
 
 }
