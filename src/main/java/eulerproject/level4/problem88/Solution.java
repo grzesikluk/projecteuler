cd79ddaa154d5721922ef12ea5adbe037656a982
grzesikluk <grzesik.luk@gmail.com>
2017-06-05 22:03:30 +0200

    Problem 88 init.

---
+++ src/main/java/eulerproject/level4/problem88/Solution.java
@@ -0,0 +1,10 @@
+package eulerproject.level4.problem88;
+
+/**
+ * Created by lgrzesik on 05/06/17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
