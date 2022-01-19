f6914a9fd18617f41a5db5894901c31fa6aee81c
Lukasz Grzesik <lukasz.grzesik@nexthink.com>
2022-01-19 09:17:21 +0100

    Add more code.

--- src/main/java/eulerproject/level5/problem107/solution.py
+++ src/main/java/eulerproject/level5/problem107/solution.py
@@ -2,6 +2,26 @@ def solution():
     return 1;
 
 
+def readFile(filename):
+    graphArray = []
+    file = open(filename, "r")
+
+    for line in file.readlines():
+        numbers = [-1 if strNumber == "-" else int(strNumber) for strNumber in line.strip().split(",")]
+        graphArray.append(numbers)
+
+    file.close()
+
+    return graphArray
+
+
+def countWeight(graphArray):
+    weight = 0
+    for i in range(len(graphArray)):
+        for j in range(len(graphArray[i])):
+            weight += graphArray[i][j] if graphArray[i][j] > 0 else 0
+    return weight/2
+
 
 
 if __name__ == '__main__':
--- src/main/java/eulerproject/level5/problem107/test_solution.py
+++ src/main/java/eulerproject/level5/problem107/test_solution.py
@@ -1,10 +1,25 @@
 import unittest
+
 import solution
+
 
 class TestProblem(unittest.TestCase):
 
+    def testRead(self):
+        graphArray = solution.readFile("test_network.txt")
+        self.assertEqual(-1, graphArray[0][0])
+        self.assertEqual(16, graphArray[0][1])
+        self.assertEqual(12, graphArray[0][2])
+        self.assertEqual(17, graphArray[1][3])
+        self.assertEqual(-1, graphArray[6][6])
+
+    def testWeight(self):
+        graphArray = solution.readFile("test_network.txt")
+        self.assertEqual(243, solution.countWeight(graphArray))
+
     def test(self):
         self.assertEqual(solution.solution("./test_network.txt"), 150)
+
 
 if __name__ == '__main__':
     unittest.main()
